1f0c349d3096bdc740e20561bb68f9e9cb324e03
james <jamesomare922@gmail.com>
2024-06-20 22:22:39 +0300

    updated register serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -2,16 +2,24 @@ from rest_framework import serializers
 from .models import UserData
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length = 60, min_length = 8, write_only = True)
 
     class Meta:
         model = UserData
-        fields = ["id", "email", "name", "password"]
+        fields = ['email', 'username', 'password']
 
+    def validate(self, attrs):
+        username = attrs.get('username', '')
+    
+        # check if username is alphanumeric
+        if not username.isalnum():
+            raise serializers.ValidationError(
+                'The username should only contain alphanumeric characters')
+
+        
+        return attrs 
+    
     def create(self, validated_data):
-        user = UserData.objects.create(email=validated_data['email'],
-                                       name=validated_data['name']
-                                         )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+        return UserData.objects.create_user(**validated_data)
